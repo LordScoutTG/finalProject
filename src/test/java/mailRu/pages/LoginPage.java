455d50ff2623ef2f5c6833f9a012d81bd2f4fff0
Roman Dubakin <trading.sev@gmail,com>
2023-10-05 22:57:58 +0300

    final mailru commit

--- src/test/java/mailRu/pages/LoginPage.java
+++ src/test/java/mailRu/pages/LoginPage.java
@@ -6,28 +6,34 @@ import org.openqa.selenium.By;
 import static mailRu.helpers.WebDriverContainer.setDriver;
 
 public class LoginPage extends TestBase {
+    private static final String correctLoginEmail = "testfortest.2024";
+    private static final String correctLoginPassword = "3iApaPlOtU*2";
+
+
     private static final By loginButton = By.cssSelector("[data-testid='enter-mail-primary']");
     private static final By userNameInputField = By.cssSelector("[name='username']");
     private static final By enterPasswordButton = By.cssSelector("[class='submit-button-wrap'] [type='submit']");
     private static final By passwordInputField = By.cssSelector("[name='password']");
     private static final By submitAndLoginButton = By.cssSelector("[class='login-row'] [type='submit']");
-    private static final String correctLoginEmail = "testfortest.2024";
-    private static final String correctLoginPassword = "3iApaPlOtU*2";
 
 
     private static void loginButtonClick() {
+        LOG.info("Clicking on button login");
         setDriver().findElement(loginButton).click();
     }
 
     private static void emailInput() {
+        LOG.info("Filling login email");
         setDriver().findElement(userNameInputField).sendKeys(correctLoginEmail);
     }
 
     private static void enterPasswordButtonClick() {
+        LOG.info("Clicking on button to open password field");
         setDriver().findElement(enterPasswordButton).click();
     }
 
     private static void passwordInput() {
+        LOG.info("Filling login password");
         setDriver().findElement(passwordInputField).sendKeys(correctLoginPassword);
     }
 
@@ -37,6 +43,7 @@ public class LoginPage extends TestBase {
         emailInput();
         enterPasswordButtonClick();
         passwordInput();
+        LOG.info("Submit and login");
         setDriver().findElement(submitAndLoginButton).click();
 
     }
--- src/test/java/mailRu/pages/MailMainPage.java
+++ src/test/java/mailRu/pages/MailMainPage.java
@@ -6,45 +6,56 @@ import org.openqa.selenium.By;
 import static mailRu.helpers.WebDriverContainer.setDriver;
 
 public class MailMainPage extends TestBase {
+    private static final String recipient = "asd@asd.ru";
+    private static final String letterSubject = "Hello";
+    private static final String letterText = "How are you?";
+
+
+
     private static final By composeMailButton = By.cssSelector("[class='compose-button__txt']");
-    private static final By recepientField = By.xpath("//div[contains(@class,'contacts')]//input[contains(@class,'container')]");
+    private static final By recipientField = By.xpath("//div[contains(@class,'contacts')]//input[contains(@class,'container')]");
     private static final By subjectLetterField = By.cssSelector("[name='Subject']");
     private static final By textLetterField = By.cssSelector("[role='textbox']");
     private static final By sendLetterButton = By.cssSelector("[data-test-id='send']");
     private static final By sentLettersButton = By.xpath("//a[contains(@title,'Отправленные')]");
-    private static final By sentLetterTitle = By.cssSelector("[title='asd@asd.ru']");
-    private static final String recepient = "asd@asd.ru";
-    private static final String letterSubject = "Hello";
-    private static final String letterText = "How are you?";
+    private static final By sentLetterTitle = By.cssSelector(String.format("[title='%s']", recipient));
 
     private static void composeMailButtonClick() {
+        LOG.info("Clicking on Compose letter button");
         setDriver().findElement(composeMailButton).click();
     }
 
-    private static void fillRecepientFieldText() {
-        setDriver().findElement(recepientField).sendKeys(recepient);
+    private static void fillRecipientFieldText() {
+        LOG.info("Filling recipient email");
+        setDriver().findElement(recipientField).sendKeys(recipient);
     }
 
     private static void fillLetterSubjectText() {
+        LOG.info("Filling subject of email");
         setDriver().findElement(subjectLetterField).sendKeys(letterSubject);
     }
 
     private static void fillLetterTextBody() {
+        LOG.info("Filling email text");
         setDriver().findElement(textLetterField).sendKeys(letterText);
     }
 
     public static void sendLetter() {
         composeMailButtonClick();
-        fillRecepientFieldText();
+        fillRecipientFieldText();
         fillLetterSubjectText();
         fillLetterTextBody();
+        LOG.info("Sending letter");
         setDriver().findElement(sendLetterButton).click();
+        LOG.info("Closing after-window");
         setDriver().findElement(By.cssSelector("[class=\"layer-window__block\"] [class=\"button2__wrapper button2__wrapper_centered\"] [class=\"button2__ico\"]")).click();
     }
 
     public static boolean ifLetterIsSent() {
+        LOG.info("Clicking on Sent items in left menu");
         setDriver().findElement(sentLettersButton).click();
-        return setDriver().findElement(sentLetterTitle).getText().equals(recepient);
+        LOG.info("Checking, if sent letter is correct");
+        return setDriver().findElement(sentLetterTitle).getText().equals(recipient);
     }
 
 }
